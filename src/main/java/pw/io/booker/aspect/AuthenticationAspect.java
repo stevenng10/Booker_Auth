67d6d5c5dad78be569b67006bcb2f6c2bef46e42
Steven <steven.ng@pointwest.com.ph>
2018-09-03 17:20:37 +0800

    fifth commit

--- src/main/java/pw/io/booker/aspect/AuthenticationAspect.java
+++ src/main/java/pw/io/booker/aspect/AuthenticationAspect.java
@@ -1,8 +1,12 @@
 package pw.io.booker.aspect;
 
+import org.apache.log4j.Logger;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 import pw.io.booker.exceptionhandling.AuthenticationException;
@@ -13,6 +17,7 @@ import pw.io.booker.repo.AuthenticationRepository;
 public class AuthenticationAspect {
 
 	AuthenticationRepository authenticationRepository;
+	Logger logger = Logger.getLogger(AuthenticationAspect.class);
 
 	public AuthenticationAspect(AuthenticationRepository authenticationRepository) {
 		super();
@@ -25,19 +30,38 @@ public class AuthenticationAspect {
 		Object returnObject = null;
 
 		if (token == null) {
+			logger.error("Token is null! Access is denied!");
 			throw new AuthenticationException("Access Denied");
 		}
 
 		if (authenticationRepository.findByToken(token) == null) {
+			logger.error("Cannot find token in repository! Access is denied!");
 			throw new AuthenticationException("Access Denied");
 		}
 
 		try {
 			returnObject = jointpoint.proceed();
 		} catch (Throwable e) {
-			throw new AuthenticationException("Access Denied: " + e);
+			logger.error("ThrowableException Occured: " + e.getMessage());
+			throw new AuthenticationException("ThrowableException Occured: " + e.getMessage());
 		}
 
 		return returnObject;
+	}
+
+	@Before("execution(* pw.io.booker.service..*(..))")
+	public void logMethodStart(JoinPoint jointpoint) {
+		logger.info("**********************************");
+		logger.info("Start of method: ");
+		logger.info(jointpoint.getSignature().getName());
+		logger.info("**********************************");
+	}
+
+	@After("execution(* pw.io.booker.service..*(..))")
+	public void logMethodEnd(JoinPoint jointpoint) {
+		logger.info("**********************************");
+		logger.info("End of method: ");
+		logger.info(jointpoint.getSignature().getName());
+		logger.info("**********************************");
 	}
 }
